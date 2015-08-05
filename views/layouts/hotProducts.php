e18187db533084726929dbe218daeb741a62f1d9
zhangj <zhangj@pzfresh.com>
2015-08-05 13:41:07 +0800

    maximum=1.0

--- views/layouts/hotProducts.php
+++ views/layouts/hotProducts.php
@@ -3,7 +3,7 @@
     <head>
         <title>热销榜</title>
         <meta charset="UTF-8">
-        <meta name="viewport" content="initial-scale=1.0,width=device-width,maximum-scale=1,minimum-scale=1.0,user-scalable=no">
+        <meta name="viewport" content="initial-scale=1.0,width=device-width,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no">
         <link rel="stylesheet" type="text/css" href="<?php echo Yii::$app->request->hostInfo.Yii::$app->urlManager->baseUrl;?>/pzfresh/css/font-awesome.min.css">
         <link rel="stylesheet" type="text/css" href="<?php echo Yii::$app->request->hostInfo.Yii::$app->urlManager->baseUrl;?>/pzfresh/css/wxshop.css">
     </head>
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -16,7 +16,7 @@ AppAsset::register($this);
 <html lang="<?= Yii::$app->language ?>">
     <head>
         <meta charset="<?= Yii::$app->charset ?>">
-        <meta name="viewport" content="initial-scale=1.0,width=device-width,maximum-scale=1,minimum-scale=1.0,user-scalable=no">
+        <meta name="viewport" content="initial-scale=1.0,width=device-width,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no">
         <?= Html::csrfMetaTags() ?>
         <title><?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
